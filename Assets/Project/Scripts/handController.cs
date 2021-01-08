780bfbb712645a27933c09fb91ded1c3699b1d5a
Toni <antonio.prieto@enti.cat>
2021-01-08 17:56:13 +0100

    gitflow-feature-stash: UIFIX

--- Assets/Project/Scripts/handController.cs
+++ Assets/Project/Scripts/handController.cs
@@ -16,8 +16,7 @@ public class handController : MonoBehaviour
     private GameObject absorbGun;
     public GameObject noAmmoText;
     public static int currentPos;
-
-    private Transform gunSelector;
+    
 
     public GameObject purpleReloaderUI, yellowReloaderUI, redReloaderUI;
 
@@ -27,17 +26,11 @@ public class handController : MonoBehaviour
         absorbGun = transform.GetChild(1).gameObject;
         yellowGun = transform.GetChild(2).gameObject;
         redGun = transform.GetChild(3).gameObject;
-        gunSelector = GameObject.FindWithTag("gunSelector").transform;
-       /* purpleReloaderUI = GameObject.Find("purpleReloaderUI");
-        yellowReloaderUI = GameObject.Find("yellowReloaderUI");
-        redReloaderUI = GameObject.Find("redReloaderUI");*/
         currentPos = 0;
     }
 
     void Update()
     {
-        WeaponSelector();
-        
         if (Input.GetAxis("Mouse ScrollWheel") > 0f) // Rueda arriba
         {
             currentPos--;
@@ -57,6 +50,8 @@ public class handController : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.Alpha1)) currentPos = 0;
         else if (Input.GetKeyDown(KeyCode.Alpha2)) currentPos = 1;
         else if (Input.GetKeyDown(KeyCode.Alpha3)) currentPos = 2;
+        Debug.Log(currentPos);
+        WeaponSelector();
     }
 
     void WeaponSelector()
@@ -64,17 +59,14 @@ public class handController : MonoBehaviour
         if (currentPos == 0)
         {
             purpleGun.SetActive(true);
+            yellowGun.SetActive(false);
             redGun.SetActive(false);
 
             // GUN COLOR IDENTIFIER
-            purpleGunUI.color = new Color(purpleGunUI.color.r, purpleGunUI.color.g, purpleGunUI.color.b, 1f);
-            yellowGunUI.color = new Color(yellowGunUI.color.r, yellowGunUI.color.g, yellowGunUI.color.b, 0.3176471f);
-            redGunUI.color = new Color(redGunUI.color.r, redGunUI.color.g, redGunUI.color.b, 0.3176471f);
+            purpleGunUI.gameObject.SetActive(true);
+            yellowGunUI.gameObject.SetActive(false);
+            redGunUI.gameObject.SetActive(false);
 
-            // GUN SELECTOR
-            gunSelector.position = new Vector3(purpleGunUI.transform.position.x,
-                purpleGunUI.transform.position.y + 0.02f, purpleGunUI.transform.position.z);
-            
             // RELOADER UI
             purpleReloaderUI.SetActive(true);
             yellowReloaderUI.SetActive(false);
@@ -86,14 +78,11 @@ public class handController : MonoBehaviour
             purpleGun.SetActive(false);
             yellowGun.SetActive(true);
             redGun.SetActive(false);
+            
             // GUN COLOR IDENTIFIER
-            purpleGunUI.color = new Color(purpleGunUI.color.r, purpleGunUI.color.g, purpleGunUI.color.b, 0.3176471f);
-            yellowGunUI.color = new Color(yellowGunUI.color.r, yellowGunUI.color.g, yellowGunUI.color.b, 1f);
-            redGunUI.color = new Color(redGunUI.color.r, redGunUI.color.g, redGunUI.color.b, 0.3176471f);
-
-            // GUN SELECTOR
-            gunSelector.position = new Vector3(yellowGunUI.transform.position.x,
-                yellowGunUI.transform.position.y + 0.02f, yellowGunUI.transform.position.z);
+            purpleGunUI.gameObject.SetActive(false);
+            yellowGunUI.gameObject.SetActive(true);
+            redGunUI.gameObject.SetActive(false);
             
             // RELOADER UI
             purpleReloaderUI.SetActive(false);
@@ -108,13 +97,9 @@ public class handController : MonoBehaviour
             redGun.SetActive(true);
 
             // GUN COLOR IDENTIFIER
-            purpleGunUI.color = new Color(purpleGunUI.color.r, purpleGunUI.color.g, purpleGunUI.color.b, 0.3176471f);
-            yellowGunUI.color = new Color(yellowGunUI.color.r, yellowGunUI.color.g, yellowGunUI.color.b, 0.3176471f);
-            redGunUI.color = new Color(redGunUI.color.r, redGunUI.color.g, redGunUI.color.b, 1f);
-
-            // GUN SELECTOR
-            gunSelector.position = new Vector3(redGunUI.transform.position.x, redGunUI.transform.position.y + 0.04f,
-                redGunUI.transform.position.z);
+            purpleGunUI.gameObject.SetActive(false);
+            yellowGunUI.gameObject.SetActive(false);
+            redGunUI.gameObject.SetActive(true);
             
             // RELOADER UI
             purpleReloaderUI.SetActive(false);
