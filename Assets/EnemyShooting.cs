b1a3d3cf73ec41dc0941ab9019f487dc2544317a
Jose <joseubaldo.grabusqui@enti.cat>
2021-01-12 00:38:13 +0100

    New Enemy Grenade

--- Assets/EnemyShooting.cs
+++ Assets/EnemyShooting.cs
@@ -10,17 +10,20 @@ public class EnemyShooting : MonoBehaviour
     public GameObject enemyBulletAttack, enemyBullet;
     private float bulletSpeed;
 
-   [HideInInspector] public bool onArea = false;
+    [HideInInspector] public bool onArea = false;
     private float cadency; private int shootCounter; private float keepCadency;
     private float timeBtwShoots;
     public float startTimeBtwShoots;
+
+    public List<GameObject> firePoints;
+
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
         shootCounter = 2; //added
-        bulletSpeed = 2.5f;
+        bulletSpeed = 15f;
         cadency = 0;
-        keepCadency = 1;
+        keepCadency = 5;
         startTimeBtwShoots = 2f;
         timeBtwShoots = startTimeBtwShoots;
     }
@@ -32,29 +35,37 @@ public class EnemyShooting : MonoBehaviour
         {
             if (cadency <= 0)
             {
-                GameObject bullet;
-                Rigidbody2D rb;
-
-                SoundManagerScript.PlaySound("EnemyShoot");
-                if (Random.Range(0f, 100f) <= 35.0f)
-                {
-                    bullet = Instantiate(enemyBulletAttack, this.transform.position, this.transform.rotation);
-                    rb = bullet.GetComponent<Rigidbody2D>();
-                }
-                else
-                {
-                    bullet = Instantiate(enemyBullet, this.transform.position, this.transform.rotation);
-                    rb = bullet.GetComponent<Rigidbody2D>();
-                }
-                target = GameObject.FindGameObjectWithTag("Player").transform;  
-                rb.AddForce(-target.position * bulletSpeed, ForceMode2D.Impulse);
-                shootCounter--;
+                StartCoroutine(Shooting());
                 cadency = keepCadency;
             }
             else
-            {
                 cadency -= Time.fixedDeltaTime;
-            }
+
         }
+    }
+
+
+    IEnumerator Shooting()
+    {
+        GameObject bullet;
+        Rigidbody2D rb;
+        Vector2 moveDirection;
+        moveDirection = (target.transform.position - transform.position).normalized * bulletSpeed;
+        SoundManagerScript.PlaySound("EnemyShoot");
+        if (Random.Range(0f, 100f) <= 35.0f)
+        {
+            bullet = Instantiate(enemyBulletAttack, firePoints[0].transform.position, firePoints[0].transform.localRotation);
+            rb = bullet.GetComponent<Rigidbody2D>();
+        }
+        else
+        {
+
+            bullet = Instantiate(enemyBullet, firePoints[0].transform.position, firePoints[0].transform.localRotation);
+            rb = bullet.GetComponent<Rigidbody2D>();
+        }
+
+        rb.AddForce(bullet.transform.right * bulletSpeed, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(2f);
+
     }
 }
---
+++ Assets/ParabolEnemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParabolEnemy : MonoBehaviour
+{
+
+    public GameObject bullet;
+    enemyPatrol parent;
+    public float launchForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = GetComponentInParent<enemyPatrol>();
+        StartCoroutine(Shoot());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Shoot()
+    {
+        while (true)
+        {
+            Vector3 vec;
+            GameObject bulletInstantiate = Instantiate(bullet, transform.position, transform.rotation);
+            Transform positionTarget = GameObject.FindGameObjectWithTag("Player").transform;
+            if(parent.getMovingRight())
+                bulletInstantiate.GetComponent<Rigidbody2D>().velocity = new Vector2(-2, 10);
+            else
+                bulletInstantiate.GetComponent<Rigidbody2D>().velocity = new Vector2(2, 10);
+
+
+            //bulletInstantiate.GetComponent<Rigidbody2D>().AddForce(vec * launchForce, ForceMode2D.Impulse);
+            yield return new WaitForSeconds(3);
+        }
+
+    }
+}
