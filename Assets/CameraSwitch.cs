eed4315327d3ec7edf1413878e4fd27ecf4944d3
Jose <joseubaldo.grabusqui@enti.cat>
2021-01-12 19:22:01 +0100

    Change Cameras

--- Assets/CameraSwitch.cs
+++ Assets/CameraSwitch.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class CameraSwitch : MonoBehaviour
 {
+    bool nextRoom = false;
+    public Vector3 position, position2;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
-        { 
-            CameraManagment.changeCameras(new Vector3(transform.position.x + 5, transform.position.y + 5, transform.position.z - 1));
+        {
+            if (!nextRoom)
+            { 
+                CameraManagment.changeCameras(position);
+                nextRoom = true;
+            }
+            else
+            { 
+                CameraManagment.changeCameras(position2);
+                nextRoom = false;
+            }
         }
 
     }
