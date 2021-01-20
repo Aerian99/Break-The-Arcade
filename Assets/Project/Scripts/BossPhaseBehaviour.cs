cef7c0618033af2ffeb066a8eacf92a56a5424f3
Jose <joseubaldo.grabusqui@enti.cat>
2021-01-20 23:28:11 +0100

    Boss Done

--- Assets/Project/Scripts/BossPhaseBehaviour.cs
+++ Assets/Project/Scripts/BossPhaseBehaviour.cs
@@ -4,21 +4,30 @@ using UnityEngine;
 
 public class BossPhaseBehaviour : MonoBehaviour
 {
-
-    public float speed;
+    public GameObject bulletPrefab, bulletGrenade;
+    private float speed;
     Rigidbody2D EnemyRB;
+    GameObject bulletGO;
     public GameObject rightCheck, roofCheck, groundCheck;
     public LayerMask groundLayer;
     private bool facingRight = true, groundTouch, roofTouch, rightTouch;
     public float dirX = 1, DIRy = 0.25f;
     public float circleRadius;
-    enum Phases { INITPHASE, PHASE2, PHASE3}
+    SpriteRenderer renderer;
+    enum Phases { INITPHASE, PHASE2, PHASE3 }
     Phases phase;
     public int health, maxHealth;
+
+    private Vector3 startPoint;
+
+    public Transform firePoint;
+
+    private float shootForce = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        renderer = GetComponent<SpriteRenderer>();
         speed = 750;
         EnemyRB = GetComponent<Rigidbody2D>();
         health = maxHealth = 1000;
@@ -30,11 +39,11 @@ public class BossPhaseBehaviour : MonoBehaviour
     void Update()
     {
         Movement();
-        if(health < maxHealth / 4)
+        if (health < maxHealth / 4)
         {
             phase = Phases.PHASE3;
         }
-        else if(health < maxHealth / 2)
+        else if (health < maxHealth / 2)
         {
             phase = Phases.PHASE2;
         }
@@ -63,20 +72,19 @@ public class BossPhaseBehaviour : MonoBehaviour
 
     void HitLogic()
     {
-        if(rightTouch && facingRight)
+        if (rightTouch && facingRight)
         {
             Flip();
         }
-        else if(rightTouch && !facingRight)
+        else if (rightTouch && !facingRight)
         {
             Flip();
-
-
         }
-        if(roofTouch)
+        if (roofTouch)
         {
             DIRy = -0.25F;
-        }else if(groundTouch)
+        }
+        else if (groundTouch)
         {
             DIRy = 0.25f;
         }
@@ -95,29 +103,132 @@ public class BossPhaseBehaviour : MonoBehaviour
     {
         while (true)
         {
-            if(phase == Phases.INITPHASE)
+            if (phase == Phases.INITPHASE)
             {
-                //Shoot
+                renderer.color = Color.green;
+                int rand = Random.Range(0,2);
 
+                if (rand == 0)
+                {
+                    ShootRadial(30, 10f, 1500);
+                }
+                else
+                {
+                    ShootGrenade();
+                }
 
-                yield return new WaitForSeconds(5);
-            }else if(phase == Phases.PHASE2)
+                yield return new WaitForSeconds(3);
+            }
+            else if (phase == Phases.PHASE2)
             {
+                renderer.color = Color.yellow;
                 speed = 1000;
                 //Shoot
+                int rand = Random.Range(0, 2);
 
-                yield return new WaitForSeconds(2.5f);
+                if (rand == 0)
+                {
+                    StartCoroutine(ShootRadial(30, 10f, 1500, 2));
+                }
+                else
+                {
+                    StartCoroutine(ShootGrenade(2));
+                }
+                yield return new WaitForSeconds(5f);
             }
-            else if(phase == Phases.PHASE3)
+            else if (phase == Phases.PHASE3)
             {
-                //Shoot
-                //Shoot
+                renderer.color = Color.red;
+                int rand = Random.Range(0, 2);
                 speed = 1250;
+                //Shoot
+                if (rand == 0)
+                {
+                    StartCoroutine(ShootRadial(30, 10f, 1500, 3));
+                }
+                else
+                {
+                    StartCoroutine(ShootGrenade(4));
+                }
+                //Shoot
 
-                yield return new WaitForSeconds(1.5f);
+                yield return new WaitForSeconds(6f);
             }
 
         }
-        
+
+    }
+
+    void ShootRadial(int numBullets, float radius, int bulletSpeed)
+    {
+
+        startPoint = transform.position;
+        float angleStep = 360f / numBullets;
+        float angle = 0f;
+
+        for (int i = 0; i <= numBullets - 1; i++)
+        {
+            float bulletDirXPosition = startPoint.x + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
+            float bulletDirYPosition = startPoint.y + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
+
+            Vector3 bulletVector = new Vector3(bulletDirXPosition, bulletDirYPosition, 0);
+            Vector3 bulletMoveDirection = (bulletVector - startPoint).normalized * bulletSpeed;
+
+            Debug.Log(bulletMoveDirection);
+            bulletGO = Instantiate(bulletPrefab, startPoint, Quaternion.identity);
+
+            bulletGO.GetComponent<Rigidbody2D>().velocity = new Vector3(bulletMoveDirection.x, bulletMoveDirection.y, 0);
+            bulletGO.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); // Rotamos el gameobject en función de su dirección.
+            Destroy(bulletGO, 2f);
+            angle += angleStep;
+        }
+    }
+
+    IEnumerator ShootRadial(int numBullets, float radius, int bulletSpeed, int nIterations)
+    {
+
+        startPoint = transform.position;
+        float angleStep = 360f / numBullets;
+        float angle = 0f;
+        for (int y = 0; y < nIterations; y++)
+        {
+            for (int i = 0; i <= numBullets - 1; i++)
+            {
+                float bulletDirXPosition = startPoint.x + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
+                float bulletDirYPosition = startPoint.y + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
+
+                Vector3 bulletVector = new Vector3(bulletDirXPosition, bulletDirYPosition, 0);
+                Vector3 bulletMoveDirection = (bulletVector - startPoint).normalized * bulletSpeed;
+
+                Debug.Log(bulletMoveDirection);
+                bulletGO = Instantiate(bulletPrefab, startPoint, Quaternion.identity);
+
+                bulletGO.GetComponent<Rigidbody2D>().velocity = new Vector3(bulletMoveDirection.x, bulletMoveDirection.y, 0);
+                bulletGO.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); // Rotamos el gameobject en función de su dirección.
+                Destroy(bulletGO, 2f);
+                angle += angleStep;
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
+        yield return new WaitForSeconds(0);
+    }
+
+    void ShootGrenade()
+    {
+        bulletGO = Instantiate(bulletGrenade, firePoint.position, transform.rotation);
+        bulletGO.GetComponent<Rigidbody2D>().AddForce(-transform.up * shootForce, ForceMode2D.Impulse);
+        bulletGO.GetComponent<GrenadeShoot>().bulletSpeed = 15000;
+        bulletGO.GetComponent<GrenadeShoot>().numBullets = 16;
+    }
+    IEnumerator ShootGrenade(int nIterations)
+    {
+        for (int i = 0; i < nIterations; i++)
+        {
+            bulletGO = Instantiate(bulletGrenade, firePoint.position, transform.rotation);
+            bulletGO.GetComponent<Rigidbody2D>().AddForce(-transform.up * shootForce, ForceMode2D.Impulse);
+            bulletGO.GetComponent<GrenadeShoot>().bulletSpeed = 15000;
+            bulletGO.GetComponent<GrenadeShoot>().numBullets = 16;
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
