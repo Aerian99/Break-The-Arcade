090e40cc01039d548db7ba4acfd7247fad72b6bb
Sara Yang <shanshan.yang@enti.cat>
2021-01-20 21:02:59 +0100

    gitflow-feature-stash: SpaceInvaders

---
+++ Assets/SpaceInvadersTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceInvadersTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool nextRoom = false;
+    public GameObject cam1, cam2, cam3, globalLight, lightPlayer, lightEnemies, spaceInvLimit,
+        focoR, focoG, focoB;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            RenderSettings.ambientLight = Color.black;
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(true);
+            globalLight.SetActive(true); 
+            lightPlayer.SetActive(true);
+            lightEnemies.SetActive(true);
+            spaceInvLimit.SetActive(true);
+            focoR.SetActive(false);
+            focoG.SetActive(false);
+            focoB.SetActive(false);
+
+        }
+
+    }
+}
