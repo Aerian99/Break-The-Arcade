ffd004d866455188c7e3237a00126fbe68f7a9c6
Jose <joseubaldo.grabusqui@enti.cat>
2021-01-22 18:15:26 +0100

    RadialStop

--- Assets/radialEnemyShoot.cs
+++ Assets/radialEnemyShoot.cs
@@ -40,30 +40,32 @@ public class radialEnemyShoot : MonoBehaviour
 
                 for (int y = 0; y < burstSpeed; y++)
                 {
-                    for (int i = 0; i <= numBullets - 1; i++)
+                    if (!GameObject.FindGameObjectWithTag("gameController").GetComponent<GameController>().activatedAbsorb)
                     {
-                        int random = Random.Range(0, 100);
-
-
-                        float bulletDirXPosition = startPoint.x + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
-                        float bulletDirYPosition = startPoint.y + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
-
-                        Vector3 bulletVector = new Vector3(bulletDirXPosition, bulletDirYPosition, 0);
-                        Vector3 bulletMoveDirection = (bulletVector - startPoint).normalized * bulletSpeed;
-                        if (random < 15)
+                        for (int i = 0; i <= numBullets - 1; i++)
                         {
-                            bulletGO = Instantiate(bulletPacman, startPoint, Quaternion.identity);
+                            int random = Random.Range(0, 100);
+
+
+                            float bulletDirXPosition = startPoint.x + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
+                            float bulletDirYPosition = startPoint.y + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
+
+                            Vector3 bulletVector = new Vector3(bulletDirXPosition, bulletDirYPosition, 0);
+                            Vector3 bulletMoveDirection = (bulletVector - startPoint).normalized * bulletSpeed;
+                            if (random < 15)
+                            {
+                                bulletGO = Instantiate(bulletPacman, startPoint, Quaternion.identity);
+                            }
+                            else
+                            {
+                                bulletGO = Instantiate(bulletPrefab, startPoint, Quaternion.identity);
+                            }
+                            bulletGO.GetComponent<Rigidbody2D>().velocity = new Vector3(bulletMoveDirection.x, bulletMoveDirection.y, 0);
+                            bulletGO.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); // Rotamos el gameobject en función de su dirección.
+                            Destroy(bulletGO, 2f);
+                            angle += angleStep;
                         }
-                        else
-                        {
-                            bulletGO = Instantiate(bulletPrefab, startPoint, Quaternion.identity);
-                        }
-                        bulletGO.GetComponent<Rigidbody2D>().velocity = new Vector3(bulletMoveDirection.x, bulletMoveDirection.y, 0);
-                        bulletGO.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); // Rotamos el gameobject en función de su dirección.
-                        Destroy(bulletGO, 2f);
-                        angle += angleStep;
                     }
-
                     yield return new WaitForSeconds(0.4f);
                 }
 
