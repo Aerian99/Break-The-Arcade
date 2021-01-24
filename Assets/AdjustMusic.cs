b11180ce571d761c1c4bdedc7d805e9ed6059563
Sara Yang <shanshan.yang@enti.cat>
2021-01-24 20:58:07 +0100

    Music

---
+++ Assets/AdjustMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustMusic : MonoBehaviour
+{
+    public AudioSource audSrc;
+    private float musicVolume = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        audSrc.volume = musicVolume;
+    }
+
+    public void UpdateVolume(float volume)
+    {
+        musicVolume = volume;
+    }
+
+}
--- Assets/Project/MainMenuManager.cs
+++ Assets/Project/MainMenuManager.cs
@@ -5,11 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public GameObject playButton, ExitButton, text;
+    public GameObject playButton, ExitButton, optionsButton, text, slider, volumeButton, backButton;
     public void ShowButtons()
     {
         playButton.SetActive(true);
         ExitButton.SetActive(true);
+        optionsButton.SetActive(true);
         text.SetActive(false);
         SoundManagerScript.PlaySound("coin");
         GameObject.Find("p_cursor").GetComponent<CursorScript>().coinAdded = true;
@@ -19,6 +20,26 @@ public class MainMenuManager : MonoBehaviour
         SceneManager.LoadScene("MapGeneration");
     }
 
+    public void Options()
+    {
+        playButton.SetActive(false);
+        ExitButton.SetActive(false);
+        optionsButton.SetActive(false);
+        volumeButton.SetActive(true);
+        slider.SetActive(true);
+        backButton.SetActive(true);
+    }
+
+    public void Back()
+    {
+        volumeButton.SetActive(false);
+        slider.SetActive(false);
+        backButton.SetActive(false);
+        playButton.SetActive(true);
+        optionsButton.SetActive(true);
+        ExitButton.SetActive(true);
+
+    }
     public void Exit()
     {
         Application.Quit();
