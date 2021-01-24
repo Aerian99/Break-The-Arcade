8438744a295e87f3b5a80efcf0e71c92036558cd
Toni <antonio.prieto@enti.cat>
2021-01-24 18:56:46 +0100

    AbsorbBulletFix

--- Assets/magneticEffect.cs
+++ Assets/magneticEffect.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class magneticEffect : MonoBehaviour
@@ -16,6 +17,7 @@ public class magneticEffect : MonoBehaviour
         {
             other.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             other.transform.position = Vector3.MoveTowards(other.transform.position, this.transform.parent.position, Time.deltaTime * 10f);
+
             if (other.gameObject.transform.localScale.x > 0 && other.gameObject.transform.localScale.y > 0)
             {
                 other.gameObject.transform.localScale -= new Vector3(2.5f, 2.5f, 0f);
@@ -32,6 +34,7 @@ public class magneticEffect : MonoBehaviour
         if ((other.gameObject.tag == "EnemyBullet" || other.gameObject.tag == "Bullet Pacman" || other.gameObject.tag == "AlienAttack") && absorbCooldown.coolFull == false)
         {
             SoundManagerScript.PlaySound("absorbSound"); // Reproducimos el sonido de absorber al entrar en el trigger de absorción (magneticZone).
+            Destroy(other.gameObject, 0.1f);
         }
     }
 }
