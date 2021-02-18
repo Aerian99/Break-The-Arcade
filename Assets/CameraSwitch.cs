4f94589524761ea71e3e3c097541552099ba907e
Sara Yang <shanshan.yang@enti.cat>
2021-02-19 00:14:54 +0100

    Map changes

--- Assets/CameraSwitch.cs
+++ Assets/CameraSwitch.cs
@@ -5,23 +5,51 @@ using UnityEngine;
 public class CameraSwitch : MonoBehaviour
 {
     bool nextRoom = false;
-    public Vector3 newPosition, oldPosition;
     public Vector3 posFocoR, posFocoG, posFocoB;
+    public Vector3 newPos;
     public GameObject focoR, focoG, focoB;
+    public GameObject fadeImage;
     Vector3 firstPosR, firstPosG, firstPosB;
     float t;
     public bool treasureRoom;
 
+    bool startCountingFade;
+    float cd, maxCd;
+
     private void Start()
     {
         t = 1f;
+        cd = 0f;
+        maxCd = 1.1f;
+        startCountingFade = false;
+    }
+
+    private void Update()
+    {
+        if (startCountingFade)
+        {
+            cd += Time.deltaTime;
+            if (cd >= maxCd)
+            { 
+                fadeImage.GetComponent<Animator>().SetBool("fadeIn", false);
+                GameObject player = GameObject.FindGameObjectWithTag("Player");
+                player.gameObject.GetComponent<playerMovement>().enabled = true;
+                cd = 0;
+                startCountingFade = false;
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
         if (collision.gameObject.CompareTag("Player"))
         {
+            fadeImage.GetComponent<movePlayer>().newPos = newPos;
+            fadeImage.GetComponent<Animator>().SetBool("fadeIn", true);
+            startCountingFade = true;
+            collision.gameObject.GetComponent<playerMovement>().enabled = false;
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+
             if (treasureRoom)
             {
                 LEDController.isTreasureRoom = true;
@@ -30,8 +58,7 @@ public class CameraSwitch : MonoBehaviour
             {
                 firstPosR = focoR.transform.position;
                 firstPosG = focoG.transform.position;
-                firstPosB = focoB.transform.position;
-                CameraManagment.changeCameras(newPosition);
+                firstPosB = focoB.transform.position;           
                 focoR.transform.position = Vector3.Lerp(firstPosR, posFocoR, t);
                 focoG.transform.position = Vector3.Lerp(firstPosG, posFocoG, t);
                 focoB.transform.position = Vector3.Lerp(firstPosB, posFocoB, t);
@@ -39,14 +66,15 @@ public class CameraSwitch : MonoBehaviour
             }
             else
             { 
-                CameraManagment.changeCameras(oldPosition);
                 focoR.transform.position = Vector3.Lerp(posFocoR, firstPosR, t);
                 focoG.transform.position = Vector3.Lerp(posFocoG, firstPosG, t);
                 focoB.transform.position = Vector3.Lerp(posFocoB, firstPosB, t);
                 nextRoom = false;
                 LEDController.isTreasureRoom = false;
             }
+
         }
+
 
     }
 }
---
+++ Assets/movePlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePlayer : MonoBehaviour
+{
+
+    [HideInInspector]public Vector3 newPos;
+
+   public void Move()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.transform.position = newPos;
+    }
+}
