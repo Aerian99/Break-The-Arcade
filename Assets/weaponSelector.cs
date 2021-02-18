1db12c227aec24d0f0587f2241ad07f326cc5430
Toni <antonio.prieto@enti.cat>
2021-02-18 22:20:28 +0100

    gitflow-feature-stash: RadialWeaponMenu

---
+++ Assets/weaponSelector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponSelector : MonoBehaviour
+{
+    public GameObject cursor;
+    public GameObject weaponMenu;
+    void Start()
+    {
+        weaponMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            weaponMenu.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            weaponMenu.SetActive(false);
+        }
+    }
+}
