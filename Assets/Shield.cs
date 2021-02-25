848a6c7d0d4ae67916bb2c2654fa86f67b0ca2e4
Jose <joseubaldo.grabusqui@enti.cat>
2021-02-25 19:54:02 +0100

    Shield

---
+++ Assets/Shield.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "EnemyBullet")
+        {
+            Vector3 eulerAngle = new Vector3(collision.GetComponent<Transform>().rotation.eulerAngles.x, collision.GetComponent<Transform>().rotation.eulerAngles.y, collision.GetComponent<Transform>().rotation.eulerAngles.z);
+            if(collision.GetComponent<Transform>().rotation.eulerAngles.z + 180f > 360)
+            {
+                //eulerAngle.z = (collision.GetComponent<Transform>().rotation.eulerAngles.z + 180f) - 360;
+                Debug.Log(eulerAngle);
+                collision.GetComponent<Transform>().transform.Rotate(new Vector3(0,0,180));
+            }
+            collision.GetComponent<Rigidbody2D>().velocity *= -1;
+
+        }
+    }
+}
