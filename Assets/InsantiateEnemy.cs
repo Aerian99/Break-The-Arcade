142c75b0f0a15e848436205d7d63e7928ad486e0
Jose <joseubaldo.grabusqui@enti.cat>
2021-02-25 23:42:31 +0100

    Spawn Alert

---
+++ Assets/InsantiateEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class InsantiateEnemy : MonoBehaviour
+{
+    public GameObject gb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("InstantiateEnemy", 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void InstantiateEnemy()
+    {
+        Instantiate(gb, gameObject.transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+}
