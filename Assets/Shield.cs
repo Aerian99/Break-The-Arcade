6469d990b9c4df33b405dbfbbb57df82d6b67821
Jose <joseubaldo.grabusqui@enti.cat>
2021-02-26 16:27:19 +0100

    Shield bullets attack to enemies

--- Assets/Shield.cs
+++ Assets/Shield.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
+    public ParticleSystem particle;
+    public GameObject popUp;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,15 +21,18 @@ public class Shield : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "EnemyBullet")
+        if(collision.tag == "EnemyBullet" || collision.tag == "Bullet Pacman")
         {
             Vector3 eulerAngle = new Vector3(collision.GetComponent<Transform>().rotation.eulerAngles.x, collision.GetComponent<Transform>().rotation.eulerAngles.y, collision.GetComponent<Transform>().rotation.eulerAngles.z);
             if(collision.GetComponent<Transform>().rotation.eulerAngles.z + 180f > 360)
             {
-                //eulerAngle.z = (collision.GetComponent<Transform>().rotation.eulerAngles.z + 180f) - 360;
-                Debug.Log(eulerAngle);
                 collision.GetComponent<Transform>().transform.Rotate(new Vector3(0,0,180));
             }
+            collision.tag = "PurpleBullet";
+            collision.gameObject.layer = LayerMask.NameToLayer("Bullet");
+            collision.gameObject.AddComponent<purpleBulletBehaviour>();
+            collision.gameObject.GetComponent<purpleBulletBehaviour>().hitEffectPrefab = particle;
+            collision.gameObject.GetComponent<purpleBulletBehaviour>().hitDamagePopUp = popUp;
             collision.GetComponent<Rigidbody2D>().velocity *= -1;
 
         }
