ab1b4478e014fc7eb9076fa34fd508a8e065d10e
Toni <antonio.prieto@enti.cat>
2021-03-12 19:24:39 +0100

    gitflow-feature-stash: AbsorbGunFix

--- Assets/Project/Scripts/Absorb_Gun.cs
+++ Assets/Project/Scripts/Absorb_Gun.cs
@@ -47,54 +47,6 @@ public class Absorb_Gun : MonoBehaviour
 
                 if (noAmmoText.activeInHierarchy) noAmmoText.SetActive(false);
             }
-
-
-
-            /*if (firstTimeAbsorb0 && handController.currentPos == 0)
-            {
-                if (playerBehaviour.bulletsPurple < playerBehaviour.MAX_PURPLE_SHOOT)
-                {
-                    playerBehaviour.bulletsPurple += 5;
-                    SoundManagerScript.PlaySound("absorbSound");
-                }
-                else
-                {
-                    firstTimeAbsorb0 = false;
-                    ammoFull0 = true;
-                }
-
-                if (noAmmoText.activeInHierarchy) noAmmoText.SetActive(false);
-            }
-
-            else if (firstTimeAbsorb1 && handController.currentPos == 1)
-            {
-                if (handController.currentPos == 1 && playerBehaviour.bulletsYellow < playerBehaviour.MAX_YELLOW_SHOOT)
-                { 
-                    playerBehaviour.bulletsYellow += 3;
-                    SoundManagerScript.PlaySound("absorbSound");
-                }
-                else
-                {
-                    firstTimeAbsorb1 = false;
-                    ammoFull1 = true;
-                }
-                if (noAmmoText.activeInHierarchy) noAmmoText.SetActive(false);
-            }
-
-            else if (firstTimeAbsorb2 && handController.currentPos == 2)
-            {
-                if (handController.currentPos == 2 && playerBehaviour.bulletsShotgun < playerBehaviour.MAX_SHOTGUN_SHOOT)
-                { 
-                    playerBehaviour.bulletsShotgun +=2;
-                    SoundManagerScript.PlaySound("absorbSound");
-                }
-                else
-                {
-                    firstTimeAbsorb2 = false;
-                    ammoFull2 = true;
-                }
-                if (noAmmoText.activeInHierarchy) noAmmoText.SetActive(false);
-            }*/
             if (ammoFull)
             {
                 if (playerBehaviour.reservedAmmoPurple < playerBehaviour.MAX_BULLETS_PURPLE)
@@ -113,29 +65,6 @@ public class Absorb_Gun : MonoBehaviour
 
                 SoundManagerScript.PlaySound("absorbSound");
             }
-            /* if (ammoFull0 || ammoFull1 || ammoFull2)
-             {
-                 Debug.Log("ESTOY LLENO");
-                 if (handController.currentPos == 0 && playerBehaviour.reservedAmmoPurple < playerBehaviour.MAX_BULLETS_PURPLE)
-                 {
-                     playerBehaviour.reservedAmmoPurple +=5;
-                     if (noAmmoText.activeInHierarchy) noAmmoText.SetActive(false);
-                     SoundManagerScript.PlaySound("absorbSound");
-                 }
-                 if (handController.currentPos == 1 && playerBehaviour.reservedAmmoYellow < playerBehaviour.MAX_BULLETS_YELLOW)
-                 {
-                     playerBehaviour.reservedAmmoYellow +=3;
-                     if (noAmmoText.activeInHierarchy) noAmmoText.SetActive(false);
-                     SoundManagerScript.PlaySound("absorbSound");
-                 }
-                 if (handController.currentPos == 2 && playerBehaviour.reservedAmmoShotgun < playerBehaviour.MAX_BULLETS_SHOTGUN)
-                 {
-                     playerBehaviour.reservedAmmoShotgun +=2;
-                     if (noAmmoText.activeInHierarchy) noAmmoText.SetActive(false);
-                     SoundManagerScript.PlaySound("absorbSound");
-                 }
-             }*/
-            //Destroy(other.gameObject);
         }
     }
 }
